2178f9f569c33d9b5da637fa4bde967d708c0dc2
Anthony Dcosta <adcosta2000@gmail.com>
2022-05-10 12:24:06 -0400

    JS: Added two functions to save and get search histories from the local storage. That data will be saved in a list. HTML: Added a section for populating searched movies

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
 
 // Making references for the appropriate HTML Elements 
+var previousSearches = document.querySelector("#previous-searches");
 
+
+// Initializing necessary variables
+var currentSearchArr = []; // Array to keep track of user searches.
 
 // Get the information for movie (Streaming service, release date etc) using the previously obtained ID from GetMovieID function.
 var getMovieInfo = function(id){ 
@@ -46,7 +50,42 @@ var getMovieId = function(movieName){
    });
 };
 
-var saveSearchHistory = function(){
+// Display movies name in the search history. This function will be called in two different places:
+// When a movie name is searched by the user
+// When movie search history is loaded from localStorage (See: loadSearchHistory function)
+var displaySearchedMovie = function(movieName){
+      var movieNameItem = document.createElement("li");
+      movieNameItem.classList = "list-item box";
+      movieNameItem.textContent = movieName;
+      previousSearches.appendChild(movieNameItem);
+};
 
-}
 
+// This function will take the currentSearchArr and save it in the local storage.
+// We will call this function everytime the currentSearchArr updates. In other words:
+// Everytime the user makes a search using the form.
+
+var saveSearchHistory = function(currentSearchArr){
+   localStorage.setItem("Current-Searches", JSON.stringify(currentSearchArr));
+};
+
+
+// Load the saved search history from the localStorage. This function is going to be called
+// as soon as the page is loaded.
+var loadSearchHistory = function(){
+   // save the list of searches in a temporary variable storedSearchArr;
+   var storedSearchArr = JSON.parse(localStorage.getItem("Current-Searches"));
+   // if no previous data is saved in the local storage, just initialize an emtpy list for current searches.
+   if (!storedSearchArr) {
+      currentSearchArr = [];
+   } else {
+      // If there is previous data, then append them in the currSearchArr;
+      currentSearchArr = currentSearchArr.concat(storedSearchArr);
+   }
+   // append all the movie names stored in the list through a for loop
+   for (var i=0;i<currentSearchArr.length;i++){
+      displaySearchedMovie(currentSearchArr[i]);
+   };
+};
+
+loadSearchHistory();
