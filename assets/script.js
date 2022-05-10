ef2db7f759ef50415fb114c5855b1cb8ae83de59
Anthony Dcosta <adcosta2000@gmail.com>
2022-05-10 01:47:58 -0400

    set up function for getting streaming services for given movie

---
+++ assets/script.js
@@ -0,0 +1,49 @@
+// fetch the streaming services given a movie.
+
+
+// Get the information for movie (Streaming service, release date etc) using the previously obtained ID from GetMovieID function.
+var getMovieInfo = function(id){
+   // initialize an array to hold all the streaming services
+   var streamServiceArr = [];
+   var apiUrl = "https://api.watchmode.com/v1/title/" + id +"/sources/?apiKey=aLmqepfkqFpg8Bt9eHTBrVrvxQChgOYWAAUXD2io";
+
+   fetch(apiUrl).then(function(response){
+      if(response.ok){
+         response.json().then(function(data){
+            for(var i=0;i<data.length; i++){
+               if (!streamServiceArr.includes(data[i].name)){
+                  streamServiceArr.push(data[i].name);
+               }
+            };
+         });
+      } else {
+         
+         alert("Streaming Source not Found!");
+      }
+   })
+   console.log(streamServiceArr);
+};
+
+// Get the WathMode ID for the movie through this function
+var getMovieId = function(movieName){
+   var apiUrl = "https://api.watchmode.com/v1/search/?apiKey=aLmqepfkqFpg8Bt9eHTBrVrvxQChgOYWAAUXD2io&search_field=name&search_value=" + movieName;
+
+   fetch(apiUrl).then(function(response){
+      // check if the response is ok
+      if(response.ok){
+         response.json().then(function(data){
+            // if there are multiple titles containing the movie name. Use a loop to show it all.
+            console.log(data.title_results[0].id);
+            var movieId =  data.title_results[0].id;
+            getMovieInfo(movieId);
+            
+         });
+      } else {
+         // This will be replaced with modal
+         alert("Something went wrong!");
+      }
+   });
+};
+
+
+
